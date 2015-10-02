fbed9a0ef434fd2ced77e4b2879256c11478c907
Piercing <merlosalbarracin@gmail.com>
2015-10-02 19:55:39 +0200

    modifico jwtAuth.js

---
+++ servidor/lib/jwtAuth.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Your utility library for express
+ */
+
+var jwt = require('jsonwebtoken');
+var configJWT = require('../local_config').jwt;// cojo sólo 'jwt' de local_config.js
+
+/**
+ * JWT auth middleware for use with Express 4.x.
+ *
+ * @example
+ * app.use('/api-requiring-auth', jwtAuth());
+ *
+ * @returns {function} Express 4 middleware
+ */
+
+// middleware que comprueba cada petición y
+// y verifica que el token que nos es válido
+module.exports = function () {
+
+    return function (req, res, next) {
+
+        // check header or url parameters or post parameters for token
+        var token = req.body.token ||
+            req.query.token ||
+            req.headers['x-access-token'];
+
+        // decode token
+        if (token) {
+
+            // verifies secret and checks exp
+            jwt.verify(token, configJWT.secret, function (err, decoded) {
+                if (err) {
+                    return res.json({ok: false, error: {code: 401, message: 'Failed to authenticate token.'}});
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            // if there is no token return error
+            return res.status(403).json({
+                ok: false,
+                error: {code: 403, message: 'No token provided.'}
+            });
+        }
+    };
+};
